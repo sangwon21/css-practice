38641b0c79c41dc2e0fda229c651f03c4c5e665b
sangwon21 <junothyme@gmail.com>
2020-08-03 07:22:25 +0900

    feat: 창 크기 resize될 때, 이벤트 처리
    - 창 크기가 resize될 때, 처리하는 함수를 작성

--- scroll/wall3D.js
+++ scroll/wall3D.js
@@ -2,8 +2,15 @@
   const houseElem = document.querySelector(".house");
   let maxScrollValue = document.body.offsetHeight - window.innerHeight;
 
+  const resizeHandler = () => {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  };
+
   window.addEventListener("scroll", function () {
     const zMove = (pageYOffset / maxScrollValue) * 980 - 490;
     houseElem.style.transform = `translateZ(${zMove}vw)`;
   });
+
+  window.addEventListener("resize", resizeHandler);
+  resizeHandler();
 })();
